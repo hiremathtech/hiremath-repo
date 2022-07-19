afca80020fddc5c31ca2695b9b6054d99e742f64
vijay3347 <109087114+vijay3347@users.noreply.github.com>
2022-07-19 19:34:43 +0530

    Update order.java

--- order.java
+++ order.java
@@ -3,7 +3,7 @@
 select item ="xyz";
 place order="yes";
 payment process="cod or credit or debit";
-
+or exit the page
 {
 sysout"place order ";
 sysout""thank you ";
@@ -12,4 +12,6 @@ sysout""thank you ";
 sysout"visit again"
 }
 open homepage again 
+
+
 
